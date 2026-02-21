b16e69640c4fd03e8384f1c506ef315531d9eeeb
Yashraj0912 <yashrajsinghyadav09@gmail.com>
2026-02-21 13:33:32 +0530

    inserting the data

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
     private static final String url = "jdbc:mysql://127.0.0.1:3306/school";
     private static final String user ="root";
@@ -14,18 +16,46 @@ public class Main {
     try {
         Connection connection = DriverManager.getConnection(url, user,password);
         Statement statement = connection.createStatement();
-         String str = "SELECT * FROM student";
-         ResultSet result = statement.executeQuery(str);
+
+
+         inserting in = new inserting();
+        int more =1;
+        Scanner obj = new Scanner(System.in);
+        do {
+            System.out.println("enter id");
+            int id= obj.nextInt();
+            obj.nextLine();
+            System.out.println("enter name ");
+            String name = obj.nextLine();
+            System.out.println("enter age ");
+            int age = obj.nextInt();
+            System.out.println("enetr the marks");
+            float marks = obj.nextFloat();
+            if(in.insert(statement,id,name,age,marks)>0){
+                System.out.println("data entered successfully");
+            }
+            else {
+                System.out.println("data not enterd ");
+            }
+            System.out.println("do you want to enter more data : press 1 , else any button");
+             more = obj.nextInt();
+        }while(more ==1);
 
          //loop to print
-
-        while(result.next()){
-            int id = result.getInt("id");
-            String name = result.getString("name");
-            int age = result.getInt("age");
-            float marks = result.getFloat("marks");
-            System.out.println("id of the student: " + id + ", name : " + name + ", age : " + age + ", marks : " + marks);
+        String str = "SELECT * FROM student";
+        System.out.println("do you wish to see your data press 1 ");
+        ResultSet result = statement.executeQuery(str);
+        int wish = obj.nextInt();
+        if (wish==1) {
+            while (result.next()) {
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                int age = result.getInt("age");
+                float marks = result.getFloat("marks");
+                System.out.println("id of the student: " + id + ", name : " + name + ", age : " + age + ", marks : " + marks);
+            }
         }
+        System.out.println("have a nice day");
     } catch (SQLException e) {
         System.out.println(e.getMessage());
     }
---
+++ src/inserting.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class inserting {
+   public int insert(Statement s, int id , String name , int age , float marks){
+       try {
+           String str = String.format("INSERT INTO student(id,name,age,marks) VALUES(%o,'%s',%o,%f)", id, name, age, marks);
+           return s.executeUpdate(str);
+       }
+       catch (SQLException e){
+           System.out.println(e.getMessage());
+       }
+       return 0;
+   }
+
+
+}
